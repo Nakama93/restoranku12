382bdb92fc2b62ec60db53b344898460dd9fd464
Wawan Halwani <wandagaint20@gmail.com>
2025-11-17 14:08:34 +0700

    feat:create model

--- restoranku/app/Models/Order.php
+++ restoranku/app/Models/Order.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['order_code','user_id','subtotal','tax',
+    'grandtotal','status','table_number','payment_method','note','created_at','updated_at'];
+    protected $dates=['deleted_at'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems(){
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- restoranku/app/Models/OrderItem.php
+++ restoranku/app/Models/OrderItem.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderItem extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['order_id','item_id','quantity','price','tax','total_price','created_at','update_at'];
+    protected $dates = ['deleted_at'];
+
+
 }
