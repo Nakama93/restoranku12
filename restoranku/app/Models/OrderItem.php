d007a9b6c3e7507878082a94c85ed49b5efc5384
Wawan Halwani <wandagaint20@gmail.com>
2025-11-17 14:10:02 +0700

    add orderitem

--- restoranku/app/Models/OrderItem.php
+++ restoranku/app/Models/OrderItem.php
@@ -12,5 +12,12 @@ class OrderItem extends Model
     protected $fillable = ['order_id','item_id','quantity','price','tax','total_price','created_at','update_at'];
     protected $dates = ['deleted_at'];
 
-
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+  public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }   
 }
