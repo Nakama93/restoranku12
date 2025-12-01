532046ec72f7a004dd7cef2f693c47a585ada107
Wawan Halwani <wandagaint20@gmail.com>
2025-12-01 13:28:29 +0700

    feat: implement checkout process with order storage and validation

--- restoranku/app/Http/Controllers/MenuController.php
+++ restoranku/app/Http/Controllers/MenuController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use App\Models\Item;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Support\Facades\Validator;
 
 class MenuController extends Controller
 {
@@ -20,6 +24,8 @@ class MenuController extends Controller
             return view('customer.menu', ['items' => $items, 'tableNumber' => $tableNumber]);
     }
 
+
+    // Cart
     public function cart()
     {
         $cart = Session::get('cart');
@@ -106,6 +112,92 @@ class MenuController extends Controller
         Session::flash('success', 'Keranjang berhasil dikosongkan');
 
         return redirect()->route('cart')->with('success', 'Keranjang berhasil dikosongkan');
+    }
+
+    // Checkout
+    public function checkout()
+    {
+        $cart = Session::get('cart');
+        if(empty($cart)){
+            return redirect()->route('cart')->with('error', 'Keranjang kosong, silakan tambahkan item terlebih dahulu.');
+        }
+        $tableNumber = Session::get('tableNumber');
+        return view('customer.checkout', ['cart' => $cart, 'tableNumber' => $tableNumber]);
+    }
+
+    public function storeOrder(Request $request)
+    {
+        $cart = Session::get('cart');
+        $tableNumber = Session::get('tableNumber');
+        if (empty($cart)) {
+            return redirect()->route('cart')->with('error', 'Keranjang kosong, silakan tambahkan item terlebih dahulu.');
+        }
+
+        $validator = Validator::make($request->all(), [
+            'fullname' => 'required|string|max:255',
+            'phone' => 'required|string|max:15',
+        
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('checkout')
+                ->withErrors($validator);
+        }
+
+        $total=0;
+        foreach ($cart as $item) {
+            $total += $item['price'] * $item['qty'];
+        }
+
+        $totalAmount = $total;
+        foreach ($cart as $item) {
+            $totalAmount += $item['qty'] * $item['price'];
+
+            $itemDetail = [
+                'id' => $item['id'],                
+                'price' => (int) ($item['price']+($item['price']*0.1)),
+                'quantity' => $item['qty'],
+                'name' => substr($item['name'], 0, 50),
+            ];
+        }
+
+        $user=User::firstOrCreate([
+                'fullname' => $request->input('fullname'),
+                'phone' => $request->input('phone'),
+                'role_id' => 4
+        ]);
+        
+        $order = Order::create([
+            'order_code' => 'ORD-'.$tableNumber.'-'. time(),
+            'user_id' => $user->id,
+            'subtotal' => $totalAmount,
+            'tax' => 0.1 * $totalAmount,
+            'grand_total' => $totalAmount + (0.1 * $totalAmount),
+            'status' => 'pending',
+            'table_number' => $tableNumber,
+            'payment_method' => $request->payment_method,
+            'note' => $request->note,
+        ]);
+
+        foreach ($cart as $itemId=>$item) {
+            OrderItem::create([
+                'order_id' => $order->id,
+                'item_id' => $item['id'],
+                'quantity' => $item['qty'],
+                'price' => $item['price'] * $item['qty'],
+                'tax' => 0.1 * ($item['price'] * $item['qty']),
+                'total_price' => 1.1 * ($item['price'] * $item['qty']),
+            ]);
+        }
+
+        // Setelah pesanan disimpan, kosongkan keranjang
+        Session::forget('cart');
+
+        // if ($request->payment_method == 'tunai') {
+        //     return redirect()->route('checkout.succes',['order_id'=>$order->order_code])->with('success', 'Pesanan berhasil dibuat. Silakan bayar di kasir.');
+        // }
+
+        return redirect()->route('menu')->with('success', 'Pesanan berhasil dibuat. Terima kasih!');
     }
 
 }
